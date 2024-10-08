9586900a779f3125b97a27082766b110ee17ca97
alessandro <alessandro.cerasino@gmail.com>
2024-10-08 23:44:11 +0200

    Add draft homepage

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/global.css';
+import { Raleway } from 'next/font/google'
+import Navbar from '../components/Navbar';
+
+const raleway = Raleway({subsets: ['latin']});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={raleway.className}>
+      <Navbar />
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,15 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
-import Link from 'next/link';
-import Date from '../components/date';
+import Hero from '../components/Hero';
 
-export default function Home({ allPostsData }) {
+export default function Home() {
   return (
-    <Layout home>
+    <div>
       <Head>
-        <title>{siteTitle}</title>
+        <title>Salvatore Valente Photoreporter</title>
+        <meta name='description' content='Salvatore Valente Photoreporter' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
+      <Hero heading='Salvatore Valente' message='Photoreporter'/>
+    </div>
   );
-}
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
 }
